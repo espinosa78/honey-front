97b6ed13248f7edca6a473e3145913dc870ce86a
espinosa78 <espinosa.mateo81@gmail.com>
2026-08-21 22:07:51 +0200

    feat: add Header component

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,10 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <h1 className="text-red-400">Honey Pastry</h1>
+    <>
+      <Header />
+    </>
   )
 }
 
---
+++ front/src/components/Header.jsx
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <header className="flex justify-between bg-amber-200 px-8 py-5">
+      <h1 className="text-amber-700 font-extrabold text-lg">Honey Pastry</h1>
+      <nav>
+        <ul className="flex gap-2 text-amber-700 font-bold">
+          <li>
+            <a className="cursor-pointer">About</a>
+          </li>
+          <li>
+            <a className="cursor-pointer">Products</a>
+          </li>
+          <li>
+            <a className="cursor-pointer">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
