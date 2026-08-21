7c9598fedbac8eb6313dfe4327fa8dcf96a7ea79
espinosa78 <espinosa.mateo81@gmail.com>
2026-08-21 22:10:29 +0200

    feat: add Footer component

---
+++ front/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-amber-200 px-8 py-5">
+      <p className="text-center text-amber-900">Copyright © Honey Pastry 2026</p>
+    </footer>
+  )
+}
+
+export default Footer;
--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
   return (
     <header className="flex justify-between bg-amber-200 px-8 py-5">
-      <h1 className="text-amber-700 font-extrabold text-lg">Honey Pastry</h1>
+      <h1 className="text-amber-900 font-extrabold text-lg">Honey Pastry</h1>
       <nav>
-        <ul className="flex gap-2 text-amber-700 font-bold">
+        <ul className="flex gap-2 text-amber-900 font-bold">
           <li>
             <a className="cursor-pointer">About</a>
           </li>
